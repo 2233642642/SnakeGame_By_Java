5d1eba0dfc5eb06c343c97c30354235fc567db59
Md. Ashikur Rahman Siyam <149653672+2233642642@users.noreply.github.com>
2024-06-15 23:00:11 +0600

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.snakegame;
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+    public GameFrame() {
+        this.add(new GamePanel());
+        this.setTitle("Snake Game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
